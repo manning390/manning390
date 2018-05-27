d93f3140889a35c30d692a0fd993250489c11922
Michael Manning <michaelmanning390@gmail.com>
2018-05-26 22:32:58 -0700

    Removed unnecessary comment

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,8 +23,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
 
         Route::model('slug', \App\Post::class);
